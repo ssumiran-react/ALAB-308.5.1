854b3ff101dbf89725bbe1fbae6330f2ec5f5a56
ssumiran <ssumiran@yahoo.com>
2026-05-17 20:35:33 -0400

    add Part 3 feature

--- ALAB30851_script.js
+++ ALAB30851_script.js
@@ -56,7 +56,9 @@ console.log("The string long at least in number is :",getStringLongerThan(str, 3
 //////////////////////////////////////////////
 /*
 Part 2: Thinking Methodically
-When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
+When functions are built into objects, like Arrays, they are referred 
+to as “methods” of those objects. Many methods, including Array methods, 
+require “callback functions” to determine their behavior.
 For the tasks below, use the following data to test your work:
 [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
@@ -70,6 +72,43 @@ Map the array to change the “occupation” key to “job” and increment ever
 Use the reduce method to calculate the sum of the ages.
 Then use the result to calculate the average age.
 */
+const jobs=[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+ { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+ { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+ { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+ { id: "7", name: "Bilbo", occupation: "None", age: "111" }];
+ function getDivision(num1, num0){
+    
+    return num1/num0;
+}
+const sortingAge = jobs.sort((a,b) => Number(a.age)-(Number(b.age))) ;
+const ageUnder51 = jobs.filter((a) => Number(a.age) < 51) ;
+
+const jobAgeAddOne = jobs.map(item => {
+    let newJob = [];
+    let obj = {...item};
+    
+    obj['job'] = obj['occupation'];
+    delete obj['occupation'];
+    obj.age++;
+    newJob.push(obj);
+
+    return newJob;
+});
+
+const sum = jobs.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    // Calculate average
+    const average = sum / jobs.length;
+
+console.log("Age sorted :",sortingAge);
+console.log("Age under 51 :",ageUnder51);
+console.log("Job and Age add one :",jobAgeAddOne);
+// console.log("Sum Age :",sum);
+// console.log("Sum Age :",sum);
+//console.log("Job array :",jobs);
+
+
 ////////////////////////////////////////////////
 /* Part 3: Thinking Critically
 It is important to remember that when working with objects in JavaScript, we can either pass those objects into functions by value or by reference. This important distinction changes the way that functions behave, and can have large impacts on the way a program executes.
@@ -80,7 +119,24 @@ For each of the functions above, if the object does not yet contain an age field
 Thought experiment: since the Date object is an object, what would happen if we modified it in the copy of the object created in the second function using setTime() or another method? How could we circumvent potentially undesired behavior?
 */
 
-
+function addAge(arr){
+    arr.forEach((e) => {
+        e.age++;
+    });
+    return arr;
+}
+function addAgeNewObj(arr){
+    let newObj = [];
+    arr.forEach((e) => {
+        let obj = {...e};
+        obj.age++;
+        newObj.push(obj);    
+    });
+    return newObj;
+}
+console.log("Object jobs :",jobs);
+console.log("Object copy by reference :",addAge(jobs));
+console.log("Object copy by value :",addAgeNewObj(jobs));
  
  /////////////////////////////////////////////
  /*
@@ -88,6 +144,7 @@ Thought experiment: since the Date object is an object, what would happen if we 
 Practical application of these concepts varies greatly in industry, but the core foundations are the same: functions handle repeated, specialized tasks, and methods are functions attached to specific types of objects.
 The Skills-Based Assessment (SBA) for this module will have you work on a real-world example that employs all of the tools you have learned thus far. To prepare for it, revisit your previous work as described below.
 */
+
 
 //////////////////////////////
 /*Part 5: Thinking Back
